47677ebe1da1fedcb5afbf0ddf7dbfedeb119cf6
Daniil Viktorov <daniil.viktorov@ugasoft.com>
2024-10-20 15:28:13 +0300

    Fixed startup: fixed main starter class. Temporarily commented jpa and pg dependencies

--- src/main/kotlin/ru/dnvk/kotlin20/Kotlin20Application.kt
+++ src/main/kotlin/ru/dnvk/kotlin20/Kotlin20Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Kotel20Application {
-    fun main(args: Array<String>) {
-        runApplication<Kotel20Application>(*args)
-    }
+class Kotlin20Application
+
+fun main(args: Array<String>) {
+    runApplication<Kotlin20Application>(*args)
 }
