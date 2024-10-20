d25f4a7acb5fb7370f681fc5a0516604a20da194
Daniil Viktorov <daniil.viktorov@ugasoft.com>
2024-10-20 15:35:03 +0300

    Added swagger-ui. Fixed ping controller

--- src/main/kotlin/ru/dnvk/kotlin20/controller/PingController.kt
+++ src/main/kotlin/ru/dnvk/kotlin20/controller/PingController.kt
@@ -1,8 +1,12 @@
 package ru.dnvk.kotlin20.controller
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import ru.dnvk.kotlin20.controller.interfaces.IPingController
 
+@RestController
 class PingController : IPingController {
     override fun ping(): ResponseEntity<String> {
         val answer = "Pong"
--- src/main/kotlin/ru/dnvk/kotlin20/controller/interfaces/IPingController.kt
+++ src/main/kotlin/ru/dnvk/kotlin20/controller/interfaces/IPingController.kt
@@ -1,10 +1,10 @@
 package ru.dnvk.kotlin20.controller.interfaces
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController("name")
+@Tag(name = "Ping API", description = "API для трассировки сообщений серверу")
 interface IPingController {
     @GetMapping("/ping")
     fun ping(): ResponseEntity<String>
